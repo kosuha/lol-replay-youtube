e84befe069530ea4e962b58016d49b46ac909c5c
SEONHO KIM <okeydokekim@gmail.com>
2021-05-18 14:33:30 +0900

    선수별 다운로드 폴더 만들기

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-import moviepy.editor as mp
-import pyautogui
+import os
+import crawler
+import replay_maker
+
+players = ['hide on bush', 'T1 Canna', 'T1 Teddy']
+
+def make_download_dir(players):
+    path_dir = "C:\\Users\\okeyd\\Documents\\lol-replay-youtube\\downloads"
+    file_list = os.listdir(path_dir)
+    print(file_list)
+
+    for p in players:
+        if not p in file_list:
+            os.makedirs(path_dir + "\\" + p)
 
 print("Start")
+
+make_download_dir(players)
+# crawler.downloader('hide on bush')
+# replay_maker.recorder()
