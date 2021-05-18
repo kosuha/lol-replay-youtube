4c1168b74ac0177a41b920fe905c70fb2e8130a0
SEONHO KIM <okeydokekim@gmail.com>
2021-05-18 13:41:32 +0900

    선수별로 리플레이 다운로드

--- crawler.py
+++ crawler.py
@@ -2,26 +2,30 @@
 
 from selenium import webdriver    # 라이브러리에서 사용하는 모듈만 호출
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys    # 키보드 사용
 from selenium.webdriver.support.ui import WebDriverWait   # 해당 태그를 기다림
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException    # 태그가 없는 예외 처리
 import time
 import urllib
 
-driver_file = 'C:/Users/okeyd/Documents/lol-replay-youtube/chromedriver.exe'
-options = webdriver.ChromeOptions()
-options.add_argument('headless')    # headless chrome 옵션 적용
-options.add_argument('disable-gpu')    # GPU 사용 안함
-options.add_argument('lang=ko_KR')    # 언어 설정
-driver = webdriver.Chrome(driver_file) # 옵션 적용
+options = webdriver.FirefoxOptions()
+
+driver_file = r"C:/Users/okeyd/Documents/lol-replay-youtube/geckodriver.exe"
+download_location = "C:\\Users\\okeyd\\Documents\\lol-replay-youtube\\downloads"
+
+
+options.set_preference("browser.download.folderList", 2) # 다운로드 파일을 원하는 위치로 보내기
+options.set_preference("browser.download.manager.showWhenStarting", False) # 다운로드 관리자 창 비활성화
+options.set_preference("browser.download.dir", download_location) # 경로 설정
+options.set_preference("browser.helperApps.neverAsk.saveToDisk", "doesn/matter") # 파일을 여는 데 사용할 파일 형식 묻지 않도록 MIME 설정
+
+options.add_argument('--headless')  # headless chrome 옵션 적용
+options.add_argument('--disable-gpu')   # GPU 사용 안함
+
+driver = webdriver.Firefox(executable_path = driver_file, firefox_options=options) # 옵션 적용
 
 def crawler(player):
     player_parse = urllib.parse.quote(player) # 한글 깨짐 방지
-    download_replay_tag= '.GameItemList>.GameItemWrap:nth-child(1)>div>.Content>.StatsButton>.Content>.Item>a'
-    game_type_tag= '.GameItemList>.GameItemWrap:nth-child(1)>div>.Content>.GameStats>.GameType'
-    game_length_tag= '.GameItemList>.GameItemWrap:nth-child(1)>div>.Content>.GameStats>.GameLength'
-
     url = f'https://www.op.gg/summoner/userName={player_parse}'
 
     print("-" * 100)
@@ -32,20 +36,35 @@ def crawler(player):
     try:
         # 3초간 로딩 대기
         element = WebDriverWait(driver, 3).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, download_replay_tag))
+            EC.presence_of_element_located((By.CSS_SELECTOR, '.GameItemList'))
         )
 
-        game_type = driver.find_element_by_css_selector(game_type_tag).text
-        print(game_type)
+        for i in range(20, 0, -1):
+            download_replay_tag= f'.GameItemList>.GameItemWrap:nth-child({i})>div>.Content>.StatsButton>.Content>.Item>a'
+            game_type_tag= f'.GameItemList>.GameItemWrap:nth-child({i})>div>.Content>.GameStats>.GameType'
+            game_length_tag= f'.GameItemList>.GameItemWrap:nth-child({i})>div>.Content>.GameStats>.GameLength'
+            data_game_id= driver.find_element_by_css_selector(f'.GameItemList>.GameItemWrap:nth-child({i})>div').get_attribute('data-game-id')
+            print(data_game_id)
 
-        game_length = driver.find_element_by_css_selector(game_length_tag).text
-        print(game_length)
+            game_type = driver.find_element_by_css_selector(game_type_tag).text
+            print(game_type)
+            if game_type != '솔랭':
+                continue
 
-        download_replay = driver.find_element_by_css_selector(download_replay_tag)
-        download_replay.click()
-        time.sleep(1)
-        driver.switch_to_alert().accept()
-        
+            game_length = driver.find_element_by_css_selector(game_length_tag).text
+            print(game_length)
+
+            download_replay = driver.find_element_by_css_selector(download_replay_tag)
+            download_replay.click()
+            time.sleep(1)
+
+            driver.switch_to_alert().accept()
+            time.sleep(1)
+
+            popup_close_tag= f'.DimmedBlock>div>table>tbody>tr>td>div>.Close'
+            popup_close = driver.find_element_by_css_selector(popup_close_tag)
+            popup_close.click()
+            time.sleep(1)
 
     # 예외처리
     except TimeoutException:
