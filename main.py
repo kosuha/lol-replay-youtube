9a1ef0b4db0ee062e2da12713a91ae5c122acd45
SEONHO KIM <okeydokekim@gmail.com>
2021-05-20 12:39:59 +0900

    선수의 포지션 정보 추가

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import crawler
 import replay_maker
 
-players = ['hide on bush', 'T1 Canna', 'T1 Teddy']
+players = [['Hide on bush', 3], ['T1 Canna', 1], ['T1 Teddy', 4]]
 
 # 선수의 다운로드 폴더가 없으면 생성
 def make_download_dir(players):
@@ -11,8 +11,8 @@ def make_download_dir(players):
     print(file_list)
 
     for p in players:
-        if not p in file_list:
-            os.makedirs(path_dir + "\\" + p)
+        if not p[0] in file_list:
+            os.makedirs(path_dir + "\\" + p[0])
 
 # 선수의 완료 리스트가 없으면 생성
 def make_done_list(players):
@@ -21,13 +21,15 @@ def make_done_list(players):
     print(file_list)
 
     for p in players:
-        if not f'{p}.txt' in file_list:
-            txt_name = os.path.join(path_dir, f'{p}.txt')
+        if not f'{p[0]}.txt' in file_list:
+            txt_name = os.path.join(path_dir, f'{p[0]}.txt')
             f = open(txt_name, 'w')
             f.close()
 
 print("Start")
 
+make_done_list(players)
 make_download_dir(players)
-crawler.downloader('hide on bush')
+
+crawler.downloader(players[0])
 # replay_maker.recorder()
