3755843f30682040a765748de7f38850ca9c3ffa
SEONHO KIM <okeydokekim@gmail.com>
2021-05-27 15:28:56 +0900

    썸네일 디자인 변경

--- thumbnail.py
+++ thumbnail.py
@@ -11,6 +11,8 @@ def thumbnail_maker(match_info):
     champion = match_info['champion']
     vs_player = match_info['vs_name']
     vs_champion = match_info['vs_champion']
+    badge = match_info['badge']
+    kill = match_info['kill']
 
     if player == "Hide on bush":
         player = "T1 Faker"
@@ -19,17 +21,20 @@ def thumbnail_maker(match_info):
     bright = ImageEnhance.Brightness(thumbnail_img)
     thumbnail_img = bright.enhance(0.7)
     
-    font_1_size = 150
-    font_2_size = 170
+    player_font_size = 140
+    champion_font_size = 170
 
-    if len(player) > 12:
-        font_1_size = 100
+    if len(player) > 11:
+        player_font_size = 100
 
     if len(champion) > 10:
-        font_2_size = 130
+        champion_font_size = 130
     
-    font_1 = ImageFont.truetype("fonts\Kanit-BlackItalic.ttf", font_1_size)
-    font_2 = ImageFont.truetype("fonts\PermanentMarker-Regular.ttf", font_2_size)
+    player_font = ImageFont.truetype("fonts\Kanit-BlackItalic.ttf", player_font_size)
+    champion_font = ImageFont.truetype("fonts\PermanentMarker-Regular.ttf", champion_font_size)
+    badge_font = ImageFont.truetype("fonts\Kanit-BlackItalic.ttf", 70)
+    kill_font = ImageFont.truetype("fonts\Kanit-BlackItalic.ttf", 100)
+    
 
     draw = ImageDraw.Draw(thumbnail_img)
     x = math.floor(thumbnail_img.size[0] * 0.1)
@@ -37,11 +42,34 @@ def thumbnail_maker(match_info):
     xy_margin = 100
     shadow_offset = 10
 
-    draw.text((x - shadow_offset, y - shadow_offset), player, (0,0,0), font=font_1, align='center')
-    draw.text((x, y), player, (255,255,255), font=font_1, align='center')
+    draw.text((x - shadow_offset, y - shadow_offset), player, (0,0,0), font=player_font, align='center')
+    draw.text((x, y), player, (255,255,255), font=player_font, align='center')
 
-    draw.text((x - 50 - shadow_offset, y + xy_margin - shadow_offset), f'"{champion}"', (0,0,0), font=font_2, align='center')
-    draw.text((x - 50, y + xy_margin), f'"{champion}"', (255,255,0), font=font_2, align='center')
+    draw.text((x - 50 - shadow_offset, y + xy_margin - shadow_offset), f'"{champion}"', (0,0,0), font=champion_font, align='center')
+    draw.text((x - 50, y + xy_margin), f'"{champion}"', (255,255,0), font=champion_font, align='center')
+
+    draw.text((x - shadow_offset + 155, y + xy_margin - shadow_offset + 150), f'{kill}', (0,0,0), font=kill_font, align='center')
+    draw.text((x + 150, y + xy_margin + 155), f'{kill}', (255,50,50), font=kill_font, align='center')
     
     player_parse = urllib.parse.quote(match_info['name']) # 한글 깨짐 방지
     thumbnail_img.save(f"C:\\Users\\okeyd\\Documents\\lol-replay-youtube\\img\\thumbnails\\{id}_{player_parse}.jpg","JPEG")
+
+# match_info = {
+#                     'id': 5211349961,
+#                     'name': 'Hide on bush',
+#                     'pro_name': 'T1 Faker',
+#                     'region': 'KR',
+#                     'tier': 'c',
+#                     'position': 3,
+#                     'champion': 'Viktor',
+#                     'vs_name': 'ddd',
+#                     'vs_champion': 'Le',
+#                     'team': '',
+#                     'badge': 'MVP',
+#                     'kill': 'Double Kill',
+#                     'patch': '11.10.3',
+#                     'record': False,
+#                     'upload': False
+#                 }
+
+# thumbnail_maker(match_info)
